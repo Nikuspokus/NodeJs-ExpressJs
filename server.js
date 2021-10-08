1cc21a418f271aa436ddda7eb133f179acf44e3e
Nicolas MARQUILLIER <nicolas.marquillier@sii.fr>
2021-10-08 17:00:26 +0200

    Formation - Grafikart - NodeJS / ExpressJS - FINI

--- server.js
+++ server.js
@@ -2,16 +2,15 @@ let express = require('express')
 let app = express()
 let bodyParser = require('body-parser')
 let session = require('express-session')
+const { request, response } = require('express')
 
 //Moteur de template
 app.set('view engine', 'ejs')
-console.log('1')
 
 //Middleware
 app.use('/assets', express.static('public'))
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
-console.log('2')
 app.use(session({
     secret: 'lol',
     resave: false,
@@ -44,5 +43,11 @@ app.post('/', (request, response) => {
     
 })
 
-console.log('5')
+app.get('/message/:id', (request, response) => {
+    let Message = require('./models/message')
+    Message.find(request.params.id, function (message) {
+        response.render('messages/show', {message: message})
+    })
+})
+
 app.listen(8080)
